1599edbdcbe3919dce7172119491cf431b45e307
deaton00 <dge44@cornell.edu>
2020-04-26 09:38:03 -0400

    refactored Player.points

--- player.ml
+++ player.ml
@@ -14,17 +14,13 @@ let rec p_help hnd acc=  match hnd with
   | h :: t when get_val h = 1 -> p_help t ((fst acc) + 1, snd acc)
   | h :: t -> p_help t (fst acc, snd acc + get_val h)
 
-(** [best_with_aces a b] is a helper function that returns the highest non-bust 
-    point value of a hand with [a] aces and [b] non ace points*)
-let rec best_with_aces a b = match a with
-  | 0 -> b
-  | x when x * 11 + b > 21 -> best_with_aces (x - 1) (b + 1) 
-  | x -> x * 11 + b 
-
 (** [points hnd] is the highest non-bust value of points in [hnd]*)
 let points hnd = 
   let v = p_help hnd (0, 0) in
-  best_with_aces (fst v) (snd v)
+  match fst v with
+  | 0 -> snd v
+  | x when 11 + (x - 1 + snd v) > 21 -> x + snd v
+  | x -> 11 + (x - 1 + snd v)
 
 let hand (p: player) =
   p.hand 
