5a85943a4c40f89989dd918eef34e2dd87a369ed
deaton00 <dge44@cornell.edu>
2020-05-07 14:49:37 -0400

    In step function, dealer now draws only after player is done. There is also doubling down

--- state.ml
+++ state.ml
@@ -7,34 +7,40 @@ open Parser
 type t = {deck: deck; dealer: player; player: player}
 type users = Player | Dealer
 
-let init_state =
+let init_state = 
   let d = shuffle_deck in
   let d' = draw_two_cards d in
-  let d'' = draw_two_cards (deck d') in
+  let d'' = d' |> deck |> draw_two_cards  in
   { deck = (deck d''); dealer = { hand = cards d' }; 
     player = { hand = cards d'' } }
 
+(** [dealer_draw s] is the state [s] becomes after the dealer draws their cards*)
+let rec dealer_draw s = 
+  let d = s.deck in
+  let d_points = points s.dealer in
+  if d_points >= 17 then s else
+    let r = draw_card d in
+    dealer_draw {deck = (deck r); 
+                 dealer = {hand = (cards r) @ (hand s.dealer)}; 
+                 player = s.player}
+
 let step s cmd = 
-  let dealer = s.dealer in
-  let d_points = points dealer in
   let d = s.deck in
   let r = draw_card d in
-  let r' = draw_card (deck r) in
-  match d_points with
-  | n when n < 17 ->
-    if cmd = Hit then
-    { deck = (deck r'); dealer = { hand = (cards r) @ (hand s.dealer) }; 
-      player = { hand = (cards r') @ (hand s.player) } }
-    else
-    { deck = (deck r); dealer = { hand = (cards r) @ (hand s.dealer) }; 
-      player = { hand = hand s.player} }
-  | _ ->
-    if cmd = Hit then
-    { deck = (deck r); dealer = { hand = hand s.dealer}; 
-      player = { hand = (cards r) @ (hand s.player)} }
-    else
-      s
-
+  match cmd with
+  | Hit -> let s' = { deck = (deck r); 
+                      dealer = s.dealer; 
+                      player = { hand = (cards r) @ (hand s.player) }} in
+    begin match points s'.player with
+      | 21 -> dealer_draw s'
+      | _ -> s' 
+    end
+  | Stand -> dealer_draw s
+  | Double -> dealer_draw { deck = (deck r); 
+                            dealer = s.dealer; 
+                            player = { hand = (cards r) @ (hand s.player) }}
+  | Quit -> failwith "unimplemented"
+  | _ -> failwith "unimplemented"
 
 let dealer s =
   s.dealer
