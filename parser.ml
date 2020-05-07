336759654a725e06ae7d80812129a300605df76b
deaton00 <dge44@cornell.edu>
2020-05-07 14:48:17 -0400

    added double down to parser

--- parser.ml
+++ parser.ml
@@ -1,8 +1,9 @@
-type commands = Stand | Hit | Quit | Unknown
+type commands = Stand | Hit | Quit | Double | Unknown
 
 let parse str =
   match String.lowercase str with
   | "stand" -> Stand
   | "hit" -> Hit
   | "quit" -> Quit
+  | "double down" -> Double
   | _ -> Unknown
