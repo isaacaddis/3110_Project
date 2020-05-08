62f43a7285edc4b6981a35e0f5f9a4cfe8514f29
deaton00 <dge44@cornell.edu>
2020-05-08 19:07:15 -0400

    Created step_round function in state.ml

--- state.ml
+++ state.ml
@@ -2,6 +2,7 @@ open Card
 open Deck
 open Player
 open Parser
+open Controller
 
 type t = {deck: deck; dealer: Player.t; player: Player.t}
 
@@ -10,7 +11,7 @@ let init_state =
   let d' = draw_two_cards d in
   let d'' = d' |> deck |> draw_two_cards  in
   { deck = (deck d''); dealer = (make_player (cards d') 50000); player = 
-    (make_player (cards d'') 500) }
+                                                                  (make_player (cards d'') 500) }
 
 (** [dealer_draw s] is the state [s] becomes after the dealer draws cards. *)
 let rec dealer_draw s = 
@@ -19,7 +20,7 @@ let rec dealer_draw s =
   if d_points >= 17 then s else
     let r = draw_card d in
     dealer_draw {deck = (deck r); 
-      dealer = (make_player (cards r @ hand s.dealer) 50000); player = s.player}
+                 dealer = (make_player (cards r @ hand s.dealer) 50000); player = s.player}
 
 let step s cmd = 
   let d = s.deck in
@@ -27,15 +28,37 @@ let step s cmd =
   match cmd with
   | Hit ->
     let new_s = { deck = (deck r); dealer = s.dealer; 
-      player = make_player (cards r @ hand s.player) (money s.player) } in
+                  player = make_player (cards r @ hand s.player) (money s.player) } in
     if points new_s.player = 21 then dealer_draw new_s
     else new_s
   | Stand -> dealer_draw s
   | Double -> 
     dealer_draw { deck = (deck r); dealer = s.dealer; 
-      player = make_player (cards r @ hand s.player) (money s.player) }
+                  player = make_player (cards r @ hand s.player) (money s.player) }
   | Quit -> failwith "unimplemented"
   | _ -> failwith "unimplemented"
+
+let x1_5 num = (1.5 *. float_of_int num) |> int_of_float
+
+(** [repl_m s m] is state [s] but with the player's money replaced with [m]*)
+let repl_m s m =
+  { deck = s.deck;
+    dealer = s.dealer;
+    player = make_player (s.player |> hand) m }
+
+(** [pm s] is the player's money in state [s]*)
+let pm s = s.player |> money
+
+let step_round s win bet = 
+  let st' = init_state in 
+  (*using init_state here is kind of sketchy because this would presumably be 
+    reading from the save file and I don't know how that works yet*)
+  match win with
+  | Blackjack -> repl_m st' (pm s + (x1_5 bet))
+  | Win  -> repl_m st' (pm s + bet)
+  | Loss -> repl_m st' (pm s - bet)
+  | Draw -> repl_m st' (pm s)
+  | Next -> failwith "player's money cannot change during a round"
 
 let dealer s =
   s.dealer
--- state.mli
+++ state.mli
@@ -13,3 +13,7 @@ val dealer: t -> Player.t
 
 (** [step s cmd] is the result of player choice [cmd] while in state [s]*)
 val step : t -> Parser.commands -> t
+
+(** [step_round s win bet] is a fresh round of blackjack, with the player's
+    money updated according to the result [win] and wager [bet]*)
+val step_round : t -> Controller.win_condition -> int -> t 
