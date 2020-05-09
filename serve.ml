f9ce74f173b4fa91ee27207d78b911d0016422f3
Isaac Addis <isaacaddis21@gmail.com>
2020-05-08 18:09:01 -0700

    client code

--- serve.ml
+++ serve.ml
@@ -10,8 +10,25 @@ let server =
     body |> Cohttp_lwt.Body.to_string >|= (fun body ->
       (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
          uri meth headers body))
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+    >>= (fun body -> Server.respond_string ~status:`OK ~body:body ())
+  in
+  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
+
+let () = Lwt_main.run server
+
+(**
+let server () =
+  let callback _conn req body =
+    let uri = req |> Request.uri |> Uri.to_string in
+    let meth = req |> Request.meth |> Code.string_of_method in
+    let headers = req |> Request.headers |> Header.to_string in
+    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
+      (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
+         uri meth headers body))
+    >>= (fun body -> Server.respond_string ~status:`OK ~body:body ())
   in
   Server.create ~mode:(`TCP (`Port 3000)) (Server.make ~callback ())
 
-let () = Lwt_main.run server
+let () = Lwt_main.run (server ())
+
+*) 
--- user.ml
+++ user.ml
@@ -3,7 +3,7 @@ open Cohttp
 open Cohttp_lwt_unix
 
 let body =
-  Client.get (Uri.of_string "http://localhost:3000") >>= fun (resp, body) ->
+  Client.get (Uri.of_string "http://localhost:8000") >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
   Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
@@ -14,3 +14,53 @@ let body =
 let () =
   let body = Lwt_main.run body in
   print_endline ("Received body\n" ^ body)
+(**
+(** [server_err s b] prints an error response with status code [s] 
+    and body [b].
+    Returns: ()
+    Effects: Prints to console *)
+let server_err status : unit = print_endline ("Err " ^ status ^ ":")
+
+
+(** [ok_response_action b] handles a server response body [b]
+    Returns: ()
+    Effects: Prints to console *)
+let ok_response_action (body: Cohttp_lwt.Body.t) : unit= 
+  failwith "kms"
+(**  print_endline (Cohttp_lwt.Body.to_string body)*)
+
+let body =
+  Client.post ~body:(Cohttp_lwt.Body.of_string "test")
+        (Uri.of_string "https://localhost:3000)") >>= 
+        fun (response, body) ->
+          match (response, body) with
+                | (_, body) -> Cohttp_lwt.Body.to_string body
+                | _ -> failwith "err"
+  (**
+  Client.post
+    ~body:(Cohttp_lwt.Body.of_string "test")
+    (Uri.of_string "http://localhost:3000")
+  >>= fun (response, body) ->
+    match response with 
+    | { Cohttp.Response.status = `OK; _ } ->
+        failwith "kill yourself";
+  (**      ok_response_action body *)
+    | { Cohttp.Response.status; _ } -> failwith "unimplemented";
+        (**
+        response |> 
+        Response.status |>
+        Code.code_of_status |>
+        string_of_int |>
+        server_err
+        *)
+    body 
+    *)
+
+
+let () =
+  print_endline "Welcome to blackjack. You take a seat at the table.";
+  print_endline "Enter your name: ";
+  let name = read_line () in
+  let body' = Lwt_main.run (body) in
+  print_endline ("Received body\n" ^ body')
+*)
